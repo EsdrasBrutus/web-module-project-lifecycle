c5200769dc24eab21a8c61288f17d787ffe90bf5
GenHero <51133244+GenHero@users.noreply.github.com>
2021-04-09 22:02:15 -0400

    completed stretch

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,21 +1,80 @@
 import './App.css';
 import axios from 'axios'
-import styled from 'styled-components'
 import React from 'react';
+import logo from './assets/githublogo.png'
+import Card from './styled/Card'
 
 const URL = 'https://api.github.com/users/';
+const myURL = URL + 'EsdrasBrutus';
+const unkownUser = {
+  "login": ' Github User not Found',
+  "id": 0,
+  "avatar_url": "https://www.aepint.nl/wp-content/uploads/2014/12/No_image_available-260x220.jpg",
+  "name": '',
+  "location": '',
+  "bio": 'please check the spelling and try again',
+  "public_repos": 0,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+}
 
 class App extends React.Component{
   constructor(){
     super();
     this.state ={
-      user: {}
+      user: {},
+      username: ''
     }
+  }
+
+  componentDidMount(){
+    axios.get(myURL)
+    .then((res)=> {
+      this.setState({...this.state,
+      user: res.data})
+     })
+    .catch(err=>console.log(err))
+  }
+
+  changeHandler = evt =>{
+    this.setState({...this.state,
+      username: evt.target.value.replace(/\s+/g, '')
+    })
+  }
+
+  fetchUser = () =>{
+    axios.get(URL +`${this.state.username}`)
+    .then(res => {
+      this.setState({ ...this.state, user: res.data })
+    })
+    .catch(this.setState({...this.state,
+      user : unkownUser})); 
   }
 
   render(){
     return(
-      <div>
+      <div className='App'>
+        <header className='header'>
+          <img src={logo} className='logo' alt='logo' />
+          <input 
+            value={this.state.username} 
+            onChange={this.changeHandler} 
+            type='text' />
+          <button onClick={this.fetchUser}>Search</button>
+        </header>
+        <Card>
+          <img className='avatar' alt='avatar' src={this.state.user.avatar_url} />
+          <h1>{this.state.user.login}</h1>
+          <h3>{this.state.user.name}</h3>
+          <h5>{this.state.user.bio}</h5>
+          <p>Located in: {this.state.user.location}</p>
+          <p>Followers: {this.state.user.followers}</p>
+          <p>Following: {this.state.user.following}</p>
+          <p>Num. of public Repos: {this.state.user.public_repos}</p>
+          <img className='graph' src={`http://ghchart.rshah.org/${this.state.user.login}`} alt={`${this.state.user.login} Github chart`} />
+        </Card>
+        
         
       </div>
     )
---
+++ github-usercard/src/styled/Card.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Card =styled.div`
+    display:flex;
+    width: 75em;
+    align-content: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+    border: 1px #d3d3d3 solid;
+    border-radius: 16px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    margin: 1.5em;
+
+`
+
+export default Card
